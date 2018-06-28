347be1e91a6d08ba31582adb692a0ac28ff9619d
ericauv <eriauv17@gmail.com>
2018-06-27 22:35:44 -0400

    Split Frames, rough
    Initial Image File Select
    Folder Select
    Open Split Frames in new Document

--- Nishika.jsx
+++ Nishika.jsx
@@ -8,12 +8,25 @@
 //
 
 var doc;
-var newApp = app;
+var docForOverlay;
 
-doc = newApp.documents.add("1000px","1000px",300,"test",NewDocumentMode.RGB);
+//the 4 frame image file
+var imageFile;
+imageFile = new File(File.saveDialog("Select the file that contains the 4 frame image"));
+
+//~ doc = app.documents.add("1000px","1000px",300,"test",NewDocumentMode.RGB);
+
+ doc = app.open (imageFile);
+
+    //User select folder for this roll of Nishika shots
+    var folderPath = Folder.selectDialog ("Select the folder for this roll of Nishika") + "/";
+    alert(folderPath);
+
 
 ExportFrames(true);
 function ExportFrames(Horizontal){
+
+    docForOverlay = app.documents.add("1000px","1000px",300,"test",NewDocumentMode.RGB);
 
     var widthFrame = doc.width/4;
     var heightFrame = doc.height/4;
@@ -32,6 +45,8 @@ function ExportFrames(Horizontal){
     var shapeRef;
     
     for(i=1;i<=4;i++){
+        
+        //Create a vertical or horizontal slice
         switch(Horizontal){
             
             case true:
@@ -50,76 +65,94 @@ function ExportFrames(Horizontal){
                 break;
            };
        
+            //Store size of current slice
             newWidth = right-left;
             newHeight = bot-top;
-            
-            
 
-            //Create rectangle coordinates that will be used as selection
+            //Create rectangle coordinates that will be used for selection of current slice
             shapeRef = [[left,top],[left,bot],[right,bot],[right,top]];
             
-            ExportSelection(newApp,doc,shapeRef,i);
+            //Export the current slice
+            ExportSelection(app,doc,shapeRef,i);
             
-            function ExportSelection(newApp,doc,shapeRef,i){
+            function ExportSelection(app,doc,shapeRef,i){
             //creates selection from passed coordinate array
             //saves selection as JPEG with file name 'img_0' + i
                 
                 var sel;
                 
-                var opt = new ExportOptionsSaveForWeb;
-                opt.includeProfile = true;
-                opt.format =SaveDocumentType.JPEG;
-                opt.quality=100;
+                //Folder that holds the individual exported frame files
+                var imgFolder;
+                imgFolder = Folder(folderPath + "images");
                 
-                                
-                                
+                 var framePath;
+                
+                app.activeDocument = doc;
+                
                 //make the selection shapeRef
                 doc.selection.select(shapeRef);
               
                 //store selection to sel
                 var sel = doc.selection;
-                var newColor = new SolidColor;
-
-                newColor.rgb.red = i*25;
-                newColor.rgb.green = i*25;
-                newColor.rgb.blue = i*25;
-                sel.fill(newColor);
                 
+//~                 //FIll Colour selection [for testing only, to be deleted]    
+//~                 var newColor = new SolidColor;
+//~                 newColor.rgb.red = i*25;
+//~                 newColor.rgb.green = i*25;
+//~                 newColor.rgb.blue = i*25;
+//~                 sel.fill(newColor);
+                
+                //Copy current selection
                 sel.copy();
-                docNew = newApp.documents.add(newWidth,newHeight,300,"img_0" +i,NewDocumentMode.RGB);
+                
+                //Paste selection to new document
+                docNew = app.documents.add(newWidth,newHeight,300,"img_0" +i,NewDocumentMode.RGB);
                 docNew.paste();
-                docNew.exportDocument (new File("C:/test/"+docNew.name+".jpg"), ExportType.SAVEFORWEB, opt);
-//~                 docNew.saveAs (docNew.path, opt);
+                
+                //Set up export options
+                var opt = new ExportOptionsSaveForWeb;
+                opt.includeProfile = true;
+                opt.format =SaveDocumentType.JPEG;
+                opt.quality=100;          
+                
+                //Export new document
+
+                //If this is the first slice being exported for this image, create 'images' sub-folder
+                if (i==1){
+
+                    if(!imgFolder.exists) imgFolder.create();
+                    };
+
+                
+                //Store the path of the frame to be exported
+                framePath = imgFolder.fsName +"/img_0"+i+".jpg";
+                docNew.exportDocument (new File(imgFolder.fsName +"/img_0"+i+".jpg"), ExportType.SAVEFORWEB, opt);
+                
+                app.activeDocument = docForOverlay;
+                
                 docNew.close(SaveOptions.DONOTSAVECHANGES);
+                
+                var sourceFile= new File(framePath);
+                var idPlc = charIDToTypeID( "Plc " );
+                var desc3 = new ActionDescriptor();
+                var idnull = charIDToTypeID( "null" );
+                desc3.putPath( idnull, sourceFile);
+                var idFTcs = charIDToTypeID( "FTcs" );
+                var idQCSt = charIDToTypeID( "QCSt" );
+                var idQcsa = charIDToTypeID( "Qcsa" );
+                desc3.putEnumerated( idFTcs, idQCSt, idQcsa );
+                executeAction( idPlc, desc3, DialogModes.NO );
+                
+                var backLayer = docForOverlay.layers.getByName("Background");
+                
+                
+                if(backLayer != null){
+                        backLayer.allLocked=false;
+                        backLayer.remove;
+                    };
             };
         };
   };
-//~ sel.fill (fillType, mode, opacity, preserveTransparency);
-
-
-//~ function CreateGuides(doc){
-//~ //Create 3 guides at 25%, 50%, 75% width of the passed doc document
-//~     
-//~     for(var i=1; i<4;i++){
-
-//~         doc.selection.resize(w,h,
-//~      
-
-//~     }    
-//~     
-//~     for(var i = 1; i<4;i++) {
-//~             doc.guides.add(Direction.VERTICAL,  i*quarter);
-//~     }
-//~     
-//~     };
-//~  mm
-//~ CreateGuides(doc);
-//~ function test(msg) {
-//~     
-//~         Window.prompt("here is the message: " + msg);
-//~     };
-
-//~ test("hello");
 
 
 //Create Splices from the guides
